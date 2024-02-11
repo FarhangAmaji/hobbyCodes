f00f03bb4105d8c06bd7a8c14b784fc6d809f4ea
FarhangAmaji <popamaji@gmail.com>
2024-02-11 09:33:04 +0330

    remove `state` column

--- vlcInstanceTitleCatcher/View.py
+++ vlcInstanceTitleCatcher/View.py
@@ -16,25 +16,23 @@ class View(QMainWindow):
 
     def updateTitleStates(self, title_states):
         self.title_states = title_states
-        self.table.setColumnCount(3)
-        self.table.setHorizontalHeaderLabels(['Title', 'State', 'Duplicates'])
+        self.table.setColumnCount(2)
+        self.table.setHorizontalHeaderLabels(['Title', 'Duplicates'])
         self.table.setRowCount(len(self.title_states))
 
         for row, (title, state_dict) in enumerate(self.title_states.items()):
             title_item = QTableWidgetItem(title)
-            state_item = QTableWidgetItem(state_dict['state'])
             duplicates_item = QTableWidgetItem()
 
             if state_dict['duplicates'] is not None:
                 duplicates_combo = QComboBox()
                 duplicates_combo.addItems(state_dict['duplicates'])
-                self.table.setCellWidget(row, 2, duplicates_combo)
+                self.table.setCellWidget(row, 1, duplicates_combo)
             else:
                 duplicates_item.setText('None')
-                self.table.setItem(row, 2, duplicates_item)
+                self.table.setItem(row, 1, duplicates_item)
 
             self.table.setItem(row, 0, title_item)
-            self.table.setItem(row, 1, state_item)
 
     def resizeEvent(self, event):
         # Resize the last column to take up the remaining space in the table
